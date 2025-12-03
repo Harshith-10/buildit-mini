8b7e1a17a100f95885d9abf4140629ba4a4dcb93
Harshith Doddipalli <hdr8712865491@gmail.com>
2025-12-03 10:28:09 +0530

    feat: Implement Google social login and update authentication dependencies.

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -7,6 +7,12 @@ export const auth = betterAuth({
       // Send an email to the user with a link to reset their password
     },
   },
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    },
+  },
 
   /** if no database is provided, the user data will be stored in memory.
    * Make sure to provide a database to persist user data **/
